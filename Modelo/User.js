9dec864d50e04f9f1e3609e532e8e56c68b6fce3
KennethHernan <kdelacruz18@autonoma.edu.pe>
2023-04-16 18:46:57 -0500

    [Modelo]

--- Modelo/User.js
+++ Modelo/User.js
@@ -1,32 +1,36 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://schaquila:sebas123@cluster0.saeuaek.mongodb.net/myDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Conexión a la base: Hecho');
-}).catch((err) => {
-  console.error('Conexión a la base Error: ', err);
-});
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+  useNewUrlParser: true,
+  
+  useCreateIndex: true,
+
+  useUnifiedTopology: true
+  
+  }).then(() => {
+    console.log('Conexión a la base: Hecho');
+  }).catch((err) => {
+    console.error('Conexión a la base Error: ', err);
+  });
+
+  const userSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 });
 
 const User = mongoose.model('User', userSchema);
